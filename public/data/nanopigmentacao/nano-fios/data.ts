80165a3dc481816b3d643b37558047a171ce14db
Davi Cortelete <111796499+Cortelete@users.noreply.github.com>
2025-08-26 22:04:07 -0300

    feat: Update Nanopigmentação to Microblading
    Renames the "Nanopigmentação" category and procedure to "Microblading" to better reflect the service and updates pricing information.

--- public/data/nanopigmentacao/nano-fios/data.ts
+++ public/data/nanopigmentacao/nano-fios/data.ts
@@ -2,10 +2,9 @@ import { Procedure } from '../../../../types';
 
 export const nanoFios: Procedure = {
   id: 'nano-fios',
-  name: 'Nanopigmentação de Sobrancelhas',
-  description: 'Técnica avançada de micropigmentação que cria fios ultra-realistas, resultando em sobrancelhas perfeitas e naturais.',
-  price: 'Em breve',
-  comingSoon: true,
+  name: 'Microblading',
+  description: 'Técnica avançada que utiliza um indutor manual para criar fios ultra-realistas, resultando em sobrancelhas perfeitas e naturais.',
+  price: 'R$ 450,00',
   details: ['Tecnologia de ponta para precisão', 'Longa duração com retoques anuais'],
   images: [
       '/data/nanopigmentacao/nano-fios/img/NF1.png',
